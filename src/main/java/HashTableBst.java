02de5e89cb38117c2ad34a9b6fc0fae4f33ca440
yogeshdhangar5213 <yogeshdhangar5213@gmail.com>
2022-10-15 06:10:23 -0700

    Added: Check Frequncy Of Word

---
+++ src/main/java/HashTableBst.java
@@ -0,0 +1,27 @@
+import java.util.HashMap;
+import java.util.Set;
+public class HashTableBst {
+    static void sentence(String inputString){
+        HashMap<String,Integer> count = new HashMap<>();
+        count = new HashMap<>();
+        String [] words = inputString.split(" ");
+        for(String word: words){
+            if(count.containsKey(word)){
+                count.put(word, count.get(word)+1);
+            }else{
+                count.put(word,1);
+            }
+        }
+        Set<String> wordInString = count.keySet();
+        System.out.println("Frequency In word 'To Be Or Not To Be' is");
+        for (String word:wordInString){
+            if(count.get(word)>1){
+                System.out.println(word+ " :"+count.get(word));
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        sentence("To Be Or Not To Be");
+    }
+}
